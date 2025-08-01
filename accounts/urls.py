70cd60c6be0da08312721882c2cdcd4be3d42840
kawsarmatubbor <kawsarmatubbordec@gmail.com>
2025-08-02 01:44:01 +0600

    Login ok

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterViewSet.as_view(), name='register'),
+    path('login/', views.LoginViewSet.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
+from . import models
 from . import serializers
 
 class RegisterViewSet(APIView):
@@ -12,3 +16,27 @@ class RegisterViewSet(APIView):
             serializer.save()
             return Response("Registration successful.")
         return Response(serializer.errors)
+    
+class LoginViewSet(APIView):
+    def get(self, request):
+        return Response("Login(GET)")
+    
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+
+        user_exists = models.CustomUser.objects.filter(username = username).exists()
+
+        if user_exists is not None:
+            return Response("Incorrect password.")
+        return Response("User does not exists.")
